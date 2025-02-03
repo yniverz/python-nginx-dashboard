36bda1515cd8d7e7f6dc46b52360bca9c5705739
yniverz <88168412+yniverz@users.noreply.github.com>
2025-02-03 10:23:09 +0100

    debugging

--- core/webserver.py
+++ core/webserver.py
@@ -39,6 +39,7 @@ class ProxyManager:
         waitress.serve(self.app, host='127.0.0.1', port=8080)
 
     def standard_error(self, error):
+        print(error)
         time.sleep(random.uniform(4, 6))
 
         # return render_template("404.html"), 404
